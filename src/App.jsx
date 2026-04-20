d7304ef950c37982fcc051c0c819ac3be247419d
miorajulia01 <miorahanitra01.com>
2026-04-20 16:13:14 +0300

    init: initialize project with react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+const App = () => {
+  return(
+    <div className="text-4xl">
+      <h1>meteo</h1>
+      <button className="btn btn-success">error</button>
+    </div>
+  )
+}
+
+export default App
